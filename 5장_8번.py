be3224da96152fa86c738247bc997ef29e7c0c3f
dasom17 <112676952+dasom17@users.noreply.github.com>
2022-11-12 23:47:56 +0900

    Add files via upload

---
+++ 5장_8번.py
@@ -0,0 +1,10 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:\Users\user\Desktop\K\1학년 2학기\기초파이썬\5장_8번.py.txt =========
+큰 원의 중심좌표 x1: 0
+큰 원의 중심좌표 y1: 0
+큰 원의 반지름: 100
+작은 원의 중심좌표 x2: 10
+작은 원의 중심좌표 y2: 10
+작은 원의 반지름: 50
